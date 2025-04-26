2ea3b759e73dcc7f8d9f55f489ee73297dda2b5d
Viacheslav Kaliak <trinidaa@gmial.com>
2025-04-26 23:10:34 +0200

    solution

--- install-app.sh
+++ install-app.sh
@@ -6,9 +6,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -yqq --no-install-recommends python3-pip
 mkdir /app
 
-git clone https://github.com/trinidaa/azure_task_12_deploy_app_with_vm_extention.git /tmp
-cp -r /tmp/azure_task_12_deploy_app_with_vm_extention.git/app/* /app/
-rm -rf /tmp/azure_task_12_deploy_app_with_vm_extention.git
+git clone https://github.com/trinidaa/azure_task_12_deploy_app_with_vm_extention.git /tmp/repo
+cp -r /tmp/repo/app/* /app/
+rm -rf /tmp/repo
 cp /app/todoapp.service /etc/systemd/system/
 chmod +x /app/start.sh
 chmod 644 /etc/systemd/system/todoapp.service
