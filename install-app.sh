ea1f47c5a63d8f3113df7a035cf9b0638bdb875d
Viacheslav Kaliak <trinidaa@gmial.com>
2025-04-26 22:35:17 +0200

    solution

--- install-app.sh
+++ install-app.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 set -ex
-apt-get update -yq
+apt update -yq
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -yqq --no-install-recommends python3-pip || {
+apt install -yqq --no-install-recommends python3-pip || {
     echo "Failed to install python3-pip"
     exit 1
 }
 rm -rf /var/lib/apt/lists/*
 mkdir -p /app
 
-git clone https://github.com/azat-co/azure_task_12_deploy_app_with_vm_extention-main.git /tmp/azure_task_12
-cp -r /tmp/azure_task_12/app/* /app
-rm -rf  /tmp/azure_task_12
+git clone "https://github.com/azat-co/azure_task_12_deploy_app_with_vm_extention-main.git" "/tmp/azure_task_12"
+cp -r /tmp/azure_task_12/app/* /app/
+rm -rf /tmp/azure_task_12
 cp /app/todoapp.service /etc/systemd/system/
 chmod +x /app/start.sh
 chmod 644 /etc/systemd/system/todoapp.service
